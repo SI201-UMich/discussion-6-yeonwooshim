d80a753c2d25ac9a63d4dddd4ec109a4a5ba460b
Yeonwoo Shim <yeonwooshim@Yeonwooui-MacBookAir.local>
2026-02-17 15:14:22 -0500

    Implement load_results to create nested race dictionary

--- discussion6.py
+++ discussion6.py
@@ -57,7 +57,20 @@ class HorseRaces:
             inner keys are (str) races, inner values are (int) race times
             EXAMPLE: {'Special Week': {'Tenno Sho Fall': 16.5, 'Tenno Sho Spring': 16.3, 'Teio Sho': 17.0}}
         '''
-        pass
+        race_dict = {}
+
+        headers = table[0]
+
+        for row in table[1:]:
+            horse_name = row[0]
+            race_dict[horse_name] = {}
+
+            for i in range(1, len(headers)):
+                race_name = headers[i]
+                race_time = float(row[i])
+                race_dict[horse_name][race_name] = race_time
+        
+        return race_dict
 
 ###############################################################################
 ##### TASK 2
